7168e3b0caf278195e0c99da31d82c5040b3fa58
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-03-09 09:39:27 +0800

    Half correct, TLE

--- HW1/2.py
+++ HW1/2.py
@@ -1,2 +1,18 @@
 from sys import stdin
+import re
+for each_line in stdin:
+    res = 0
+    splitted = re.split(r'(\*|\+)', each_line)
+    stk = []
+    for i in splitted:
+        stk.append(i)
+        if len(stk) == 3:
+            if stk[1] == '+':
+                res = int(stk[0]) + int(stk[2])
+            else:
+                res = int(stk[0]) * int(stk[2])
+            stk = []
+            stk.append(res)
+
+    print(stk[0])
 
