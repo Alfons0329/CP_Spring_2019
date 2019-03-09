df68b930f3bc1bb475379bc8b9840ecaa48ef47d
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-03-09 10:02:06 +0800

    Half correct, TLE

--- HW1/2.py
+++ HW1/2.py
@@ -1,9 +1,11 @@
 from sys import stdin
 import re
+import collections as col
+stk = col.deque()
 for each_line in stdin:
     res = 0
     splitted = re.split(r'(\*|\+)', each_line)
-    stk = []
+    stk.clear()
     for i in splitted:
         stk.append(i)
         if len(stk) == 3:
@@ -11,7 +13,7 @@ for each_line in stdin:
                 res = int(stk[0]) + int(stk[2])
             else:
                 res = int(stk[0]) * int(stk[2])
-            stk = []
+            stk.clear()
             stk.append(res)
 
     print(stk[0])
