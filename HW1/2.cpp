c355ff5b671f0a21590212ca57198fbb23ba0268
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-03-11 22:19:17 +0800

    Almost AC with C++ version

--- HW1/2.cpp
+++ HW1/2.cpp
@@ -1,26 +1,56 @@
 #include <bits/stdc++.h>
-#define MAX_N 402
+#define MAX_NN 402
+#define MAX_N 201
 using namespace std;
 
-int res[MAX_N];
-int num[MAX_N];
+int res[MAX_NN];
+int num[MAX_NN];
+int buf[MAX_NN];
 
-void str_2_big(string s, int in[MAX_N], int str_len)
+void print_num(int in[MAX_NN])
 {
-    for(int i = 0; i < MAX_N; i++)
+    int start_pos = 0;
+    // printf("res now ");
+    for(int i = 0; i < MAX_NN; i++)
     {
-        in[MAX_N - i - 1] = s[str_len - i - 1] - '0';
+        if(res[i] > 0)
+        {
+            start_pos = i;
+            break;
+        }
     }
+
+    for(int i = start_pos; i < MAX_NN; i++)
+    {
+        printf("%d", in[i]);
+    }
+    printf("\n");
+    return;
+}
+
+void str_2_big(string s, int in[MAX_NN], int str_len)
+{
+    for(int i = 0; i < str_len; i++)
+    {
+        in[MAX_NN - i - 1] = s[str_len - i - 1] - '0';
+    }
+
+    for(int i = MAX_NN - str_len; i >= 0; i--)
+    {
+        in[i] = 0;
+    }
+    // printf("from str2_big str_len %d", str_len);
+    // print_num(in);
 }
 
 void big_num_add()
 {
-    for(int i = MAX_N - 1; i >= 0; i--)
+    for(int i = MAX_NN - 1; i >= MAX_N; i--)
     {
         res[i] += num[i];
     }
 
-    for(int i = MAX_N - 1; i >= 0; i--)
+    for(int i = MAX_NN - 1; i >= MAX_N; i--)
     {
         if(res[i] >= 10)
         {
@@ -34,50 +64,39 @@ void big_num_mul()
 {
     int k = 0;
 
-    for(int i = MAX_N - 1, l = MAX_N - 1; i >= 0; i--, l--)
+    memcpy(buf, res, sizeof(res));
+    memset(res, 0, sizeof(res));
+    for(int i = MAX_NN - 1; i >= MAX_N; i--) // outer layer num
     {
-        for(int j = MAX_N - 1; j >= 0; j--)
+        for(int j = MAX_NN - 1; j >= MAX_N; j--) // inner layer res
         {
-            res[l - k] *= num[i];
-            k++;
+            if(num[i] == 0)
+            {
+                break;
+            }
+            res[i + j + 1 - MAX_NN] += buf[j] * num[i];
+            // printf("buf %d = %d, num %d = %d res %d = %d\n", j, buf[j], i, num[i], i + j +1 -MAX_NN, res[i + j + 1 - MAX_NN]);
         }
-        k = 0;
     }
 
-    for(int i = MAX_N - 1; i >= 0; i--)
+    for(int i = MAX_NN - 1; i >= 1; i--)
     {
         if(res[i] >= 10)
         {
             res[i - 1] += res[i] / 10;
+            // printf("res %d = %d inc %d to %d\n", i, res[i], res[i] / 10, res[i - 1]);
             res[i] %= 10;
         }
     }
 }
 
-void print_res()
-{
-    int start_pos = 0;
-    for(int i = 0; i < MAX_N; i++)
-    {
-        if(res[i] != 0)
-        {
-            start_pos = i;
-        }
-    }
 
-    for(int i = start_pos; i < MAX_N; i++)
-    {
-        printf("%d", res[i]);
-    }
-    printf("\n");
-    return;
-}
 int main()
 {
     string str, tmp;
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
-    int n, cnt, str_len, flag;
+    int n, cnt, flag;
     char last_oper, this_oper;
 
     while(getline(cin, str))
@@ -85,56 +104,56 @@ int main()
         n = str.size();
         cnt = 0;
         flag = 0;
-        
+
         memset(res, 0, sizeof(res));
         memset(num, 0, sizeof(num));
-        
+        memset(buf, 0, sizeof(buf));
+
         for(int i = 0; i < n; i++)
         {
-            str_len = 0;
             if(str[i] <= '9' && str[i] >= '0') // collect the operands
             {
                 cnt = i;
-                
+
                 while(str[cnt] >= '0')
                 {
                     cnt++;
-                    str_len++;
                     if(cnt == n)
                     {
                         break;
+                    
                     }
                 }
-                
+
                 tmp = str.substr(i, cnt - i);
-                i = cnt - 1;
+                i = cnt;
             }
 
             if(flag == 0) // 1st number to do
             {
-                str_2_big(tmp, res, str_len);
-                last_oper = str[i];
-                cout << "1st " << tmp << " last_oper" << last_oper << endl;
+                str_2_big(tmp, res, tmp.length());
+                this_oper = str[i];
+                // cout << "1st " << tmp << " last_oper" << this_oper << endl;
                 flag = 1;
             }
             else
             {
-                str_2_big(tmp, res, str_len);
+                str_2_big(tmp, num, tmp.length());
                 last_oper = this_oper;
                 this_oper = str[i];
-                cout << "to oper " << tmp << " last_oper" << last_oper << endl;
-            }
+                // cout << "to oper " << tmp << " last_oper" << last_oper << endl;
 
-            if(last_oper == '+')
-            {
-                big_num_add();
-            }
-            else if(last_oper == '*')
-            {
-                big_num_mul();
+                if(last_oper == '+')
+                {
+                    big_num_add();
+                }
+                else if(last_oper == '*')
+                {
+                    big_num_mul();
+                }
             }
         }
-        print_res();
-       }
+        print_num(res);
+    }
     return 0;
 }
--- HW1/check.sh
+++ HW1/check.sh
@@ -1,9 +1,12 @@
 #/bin/bash
+set -e
 python3 2.py < 2_input.txt > 2_output_py.txt
 g++ -std=c++11 2.cpp -o 2.out
 ./2.out < 2_input.txt > 2_output_cpp.txt
 
-diff_cnt=$(diff 2_output_py.txt 2_output_cpp.txt)
-if[ $diff_cnt -ne 0 ];
-    vimdiff 2_output_py.txt 2_output_cpp.txt
+diff 2_output_py.txt 2_output_cpp.txt > /dev/null
 
+if [ $? -ne 0 ]
+then
+    vimdiff 2_output_py.txt 2_output_cpp.txt
+fi
