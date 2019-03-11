9a49a75f800c73f02eb0ea88c9b97a46ef79e8e8
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-03-11 18:12:37 +0800

    AC'd with python, now try AC with C++

---
+++ HW1/2.py
@@ -0,0 +1,17 @@
+import re
+
+while True:
+    try:
+        each_line = input()
+    except EOFError:
+        break
+
+    splitted = re.split(r'(\*|\+)', each_line)
+    res = int(splitted[0])
+    for i in range(1, len(splitted), 2):
+        if splitted[i] == '*':
+            res *= int(splitted[i + 1])
+        else:
+            res += int(splitted[i + 1])
+
+    print(res)
