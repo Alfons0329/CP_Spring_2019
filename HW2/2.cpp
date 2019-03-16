d9029b5b4c299e0c8a117df8232ca3aa555299b9
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-03-16 15:33:38 +0800

    DP+Binary Search WA, keep improving

--- HW2/2.cpp
+++ HW2/2.cpp
@@ -2,6 +2,27 @@
 #define MAX_N 1000005
 using namespace std;
 
+int bin_search(int a[], int start, int end, int target)
+{
+    int mid;
+    while(start <= end)
+    {
+        mid = start + (end - start) / 2; // prevent overflow
+        if(a[mid] == target)
+        {
+            return mid;
+        }
+        else if(target < a[mid])
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+    return start;
+}
 int main()
 {
     int testcase;
@@ -12,29 +33,37 @@ int main()
         // read each testcases
         int n;
         scanf("%d",&n);
+        if(n == 0)
+        {
+            printf("0");
+            return 0;
+        }
         static int a[MAX_N];
-        int dp[MAX_N] = {0};
+        int dp[MAX_N];
         for(int i = 0 ; i < n ; i++)
         {
             scanf("%d", &a[i]);
-            dp[i] = 1;
         }
         
         // dynamic programming
         int res = 0;
-        for(int l = 0; l < n - 1; l++)
+        dp[0] = a[0];
+        for(int i = 1; i < n; i++)
         {
-            for(int r = l + 1; r < n; r++)
+            if(a[i] < dp[0])
             {
-                if(a[r] > a[l])
-                {
-                    dp[r] = max(dp[r], dp[l] + 1);
-                    res = max(res, dp[r]);
-                    // printf("al %d ar %d dpl %d dpr %d res update to %d\n", a[l], a[r], dp[l], dp[r], res);
-                }
+                dp[0] = a[i];
+            }
+            else if(a[i] >= dp[res])
+            {
+                dp[++res] = a[i];
+            }
+            else
+            {
+                dp[bin_search(dp, 0, res, a[i])] = a[i];
             }
         }
-        printf("%d\n", res);
+        printf("%d\n", res + 1);
     }
     return 0;
 }
