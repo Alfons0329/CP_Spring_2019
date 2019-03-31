8da7f9f95f5b14dfee66ce24738aecfbc50dd029
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-03-31 21:25:07 +0800

    Doing HW4/Base Station

--- HW4/2.cpp
+++ HW4/2.cpp
@@ -24,9 +24,10 @@ void line(double& x1, double& y1, double& x2, double& y2, double& x3, double& y3
     cout << x << ' ' << y << ' ' << sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1)) << '\n'; 
 }
 
-double len(double& x1, double& y1, double& x2, double& y2)
+double len(double x1, double y1, double x2, double y2)
 {
     return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
 }
 
 int main()
@@ -38,10 +39,11 @@ int main()
     while(scanf("%lf %lf %lf %lf %lf %lf", &x1, &y1, &x2, &y2, &x3, &y3) != EOF)
     {
         vector<double> l(3, 0.0f);
-        l[0] = (x1, y1, x2, y2); 
-        l[1] = (x1, y1, x3, y3); 
-        l[2] = (x3, y3, x2, y2);
+        l[0] = len(x1, y1, x2, y2); 
+        l[1] = len(x1, y1, x3, y3); 
+        l[2] = len(x3, y3, x2, y2);
         sort(l.begin(), l.end());
+        // printf("l0 %lf l1 %lf l2 %lf\n", l[0], l[1], l[2]);
 
         if(l[0] + l[1] > l[2]) // is triangle
         {
