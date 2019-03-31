2d126ee7f90f0ce923aaf7c195ae31fa82126451
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-03-31 15:52:19 +0800

    HW4/Novelist AC with stringstream, parse \r\n\t works well

--- HW4/1.cpp
+++ HW4/1.cpp
@@ -2,30 +2,23 @@
 using namespace std;
 unordered_map<string, int> m;
 vector<string> order;
-void collect(const string& str)
+void collect(string& str)
 {
-    string w_split;
-    stringstream ss(str);
-    while(ss >> w_split)
+    string::iterator it = str.begin();
+    string pu;
+    while(it != str.end())
     {
-        string::iterator it = w_split.begin();
-        string pu;
-        cout << "wsplit[" << w_split << "]\n"; 
-        while(it != w_split.end())
+        if(isalpha(*it))
         {
-            if(isalpha(*it))
-            {
-                pu += tolower(*it);
-            }
-            ++it;
+            pu += tolower(*it);
         }
-
-        if(m.count(pu) == 0 && isalpha(pu[0]))
-        {
-            order.push_back(pu);
-        }
-        m[pu]++;
+        ++it;
     }
+    if(m.count(pu) == 0 && isalpha(pu[0]))
+    {
+        order.push_back(pu);
+    }
+    m[pu]++;
 }
 
 int main()
---
+++ HW4/1_v2.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+unordered_map<string, int> m;
+vector<string> order;
+void collect(const string& str)
+{
+    string w_split;
+    stringstream ss(str);
+    while(ss >> w_split)
+    {
+        string::iterator it = w_split.begin();
+        string pu;
+        while(it != w_split.end())
+        {
+            if(isalpha(*it))
+            {
+                pu += tolower(*it);
+
+            }
+            ++it;
+        }
+
+        if(m.count(pu) == 0 && isalpha(pu[0]))
+        {
+            order.push_back(pu);
+        }
+        m[pu]++;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    string str;
+    while(getline(cin, str))
+    {
+        collect(str);
+    }
+
+    for(auto i : order)
+    {
+        cout << i << ' ' << m[i] << '\n';
+    }
+
+    return 0;
+}
