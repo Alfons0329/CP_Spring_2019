50d5f208defb7f1d11f6f175d8d49e66aca35803
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-04-16 21:02:32 +0800

    Fixing HW6/ Skyline

--- HW6/2_TLE.cpp
+++ HW6/2_TLE.cpp
@@ -20,44 +20,62 @@ void get_ans(ull& l_most, ull& r_most, vector<ull>& wh)
     ull n = v.size(), m = wh.size(), cover = 0;
     map<ull, pair<ull, ull>> wh2; // x -> (h, x + w)
     wh2[v[0].x] = make_pair(v[0].h, v[0].x + v[0].w); 
+    area += v[0].w * v[0].h;
 
     for(ull i = 1; i < n; i++)
     {
         ull occupied = 0, xx = v[i].x, hh = v[i].h, xx2 = v[i].x + v[i].w;
+        bool flg = 0;
         auto it = wh2.begin();
-        while(it -> first > xx && it -> second -> second < xx2 || it != wh2.end())
+        printf("out itf %llu, itss %llu xx %llu xx2 %llu\n", it -> first, it ->second.second, xx, xx2);
+        while(it != wh2.end())
         {
+            printf("itf %llu, itss %llu xx %llu xx2 %llu\n", it -> first, it ->second.second, xx, xx2);
+            if(it -> first < xx && it -> second.second > xx2)
+            {
+                flg = 1;
+                break;
+            }
+            if(it -> first < xx2 && it -> second.second > xx)
+            {
+                occupied = min(xx2 - it -> first, it -> second.second - it -> first); 
+            }
             it++;
-            occupied += it -> second -> second - it -> first;
         }
-        area += (ww - occupied) * hh;
+
+        if(!flg)
+        {
+            wh2[xx] = make_pair(hh, xx2);
+            area += (xx2 - xx - occupied) * hh;
+            printf("xx2 %llu xx %llu occupied %llu hh %llu area %llu\n", xx2, xx, occupied, hh, area);
+        }
     }
 
-    ull local_start = l_most, local_max = 0;
-    int flg = 0;
-    for(ull i = l_most; i <= r_most; i++)
-    {
-        if(wh[i - l_most] == 0)
-        {
-            if(flg == 1)
-            {
+    /*ull local_start = l_most, local_max = 0;
+      int flg = 0;
+      for(ull i = l_most; i <= r_most; i++)
+      {
+      if(wh[i - l_most] == 0)
+      {
+      if(flg == 1)
+      {
 
-                len += (i - local_start) + local_max * 2;
-                local_max = 0;
-                local_start = l_most;
-                flg = 0;
-            }
-        }
-        else
-        {
-            if(local_start == l_most && i != local_start)
-            {
-                local_start = i;
-            }
-            local_max = max(wh[i - l_most], (ull)local_max);
-            flg = 1;
-        }
-    }
+      len += (i - local_start) + local_max * 2;
+      local_max = 0;
+      local_start = l_most;
+      flg = 0;
+      }
+      }
+      else
+      {
+      if(local_start == l_most && i != local_start)
+      {
+      local_start = i;
+      }
+      local_max = max(wh[i - l_most], (ull)local_max);
+      flg = 1;
+      }
+      }*/
 
     cout << len + 1  << ' ' << area;
 }
