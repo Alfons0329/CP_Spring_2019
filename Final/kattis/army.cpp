322c74a4ff837934f9a4f8b7d4fc6f56e0dab157
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-06-07 23:32:40 +0800

    Doing kattis

---
+++ Final/kattis/army.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool cmp(int a, int b)
+{
+    return a > b;
+}
+int main()
+{
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int T, space, g_sz, m_sz;
+    cin >> T;
+    while(T--)
+    {
+        cin >> g_sz >> m_sz;
+        vector<int> god, mecha;
+
+        int tmp;
+        for(int i = 0; i < g_sz; i++)
+        {
+            cin >> tmp;
+            god.push_back(tmp);
+        }
+
+        for(int i = 0; i < m_sz; i++)
+        {
+            cin >> tmp;
+            mecha.push_back(tmp);
+        }
+        
+        sort(god.begin(), god.end(), cmp);
+        sort(mecha.begin(), mecha.end(), cmp);
+        // battle
+        
+        while(mecha.size() && god.size())
+        {
+            int m = mecha.back();
+            int g = god.back();
+            // printf("mecha size %d god size %d m %d g %d \n", mecha.size(), god.size(), m, g);
+            
+            if(m > g)
+            {
+                god.pop_back();
+            }
+            else if(m <= g)
+            {
+                mecha.pop_back();
+            }
+        }
+
+        int m = mecha.size();
+        int n = god.size();
+
+        if(m == 0 && n == 0)
+        {
+            cout << "uncertain" << '\n';
+        }
+        else if(m == 0 && n)
+        {
+            cout << "Godzilla" << '\n';
+        }
+        else if(m && n == 0)
+        {
+            cout << "MechaGodzilla" << '\n';
+        }
+        else
+        {
+            cout << "uncertain" << '\n';
+        }
+    }
+    return 0;
+}
