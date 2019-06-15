8d98f183a3bd4b554c21787fbeb41ac6e6b5de60
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-06-15 17:24:11 +0800

    Doing kattis

--- Final/kattis/death.cpp
+++ Final/kattis/death.cpp
@@ -3,6 +3,37 @@ using namespace std;
 
 int main()
 {
-
+    string op;
+    int on1, on2;
+    float total = 0.0f, avg;
+    int shares;
+    while(cin >> op)
+    {
+        if(op == "buy")
+        {
+            cin >> on1 >> on2;
+            shares += on1;
+            total += on1 * on2;
+            avg = total / (float) shares; 
+        }
+        else if(op == "sell")
+        {
+            cin >> on1 >> on2;
+        
+        }
+        else if(op == "split")
+        {
+        
+        }
+        else if(op == "merge")
+        {
+        
+        }
+        else if(op == "die")
+        {
+        
+        }
+    
+    }
     return 0;
 }
