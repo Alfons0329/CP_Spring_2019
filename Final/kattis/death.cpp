40b5bf4b6685207d9467bb3e164173b7861908d9
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-06-15 19:03:58 +0800

    Doing kattis

--- Final/kattis/death.cpp
+++ Final/kattis/death.cpp
@@ -3,44 +3,50 @@ using namespace std;
 
 int main()
 {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
     string op;
     int on1, on2;
-    float total = 0.0f, avg = 0;
+    double total = 0.0f, avg = 0;
     int shares = 0;
     while(cin >> op)
     {
         if(op == "buy")
         {
             cin >> on1 >> on2;
+            total = shares * avg + (double)(on1 * on2);
             shares += on1;
-            total += on1 * on2;
-            avg = total / (float) shares; 
+            avg = total / (double) shares; 
+            //printf("shares in buy %d total %f avg %f\n", shares, total, avg);
         }
         else if(op == "sell")
         {
             cin >> on1 >> on2;
             shares -= on1;
+            //printf("shares in sell %d total %f avg %f\n", shares, total, avg);
         }
         else if(op == "split")
         {
             cin >> on1;
             shares *= on1;
+            avg /= on1;
+            //printf("shares in split %d total %f avg %f\n", shares, total, avg);
         }
         else if(op == "merge")
         {
             cin >> on1;
-            avg *= on1;
             shares /= on1; 
+            avg *= on1;
+            //printf("shares in merge %d total %f avg %f\n", shares, total, avg);
         }
         else if(op == "die")
         {
             cin >> on1;
-            float profit = on1 - avg; 
-            float final_ans = shares * (max(0.0f, on1 - profit * 0.3f));
-            printf("%.6f\n", final_ans);
+            double profit = ((double)on1 - (double)avg) < 0.0f ? 0 : ((double)on1 - (double)avg); 
+            double final_ans = shares * (max(0.0, on1 - profit * 0.3));
+            printf("%.5lf\n", final_ans);
         }
-
-        printf("op %s on1 %d on2 %d, shares now %f, avg %f \n", op.c_str(), on1, on2, shares, avg);    
     }
     return 0;
 }
