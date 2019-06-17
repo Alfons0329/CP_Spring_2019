bd66d71919979ff0238da203fc87d9f4c36a9aca
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-06-17 12:14:09 +0800

    Prototype of these 2 problems, not AC yet

---
+++ Final/kattis/sylvester.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ull unsigned long long
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        ull n, x, y, w, h;
+        cin >> n >> x >> y >> w >> h;
+
+    }
+    return 0;
+}
