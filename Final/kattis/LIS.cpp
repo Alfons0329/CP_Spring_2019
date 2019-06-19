70314b3e385bc21dd01f995be025aa8bafcf209d
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-06-19 11:01:46 +0800

    TLE with N^2 method

--- Final/kattis/LIS.cpp
+++ Final/kattis/LIS.cpp
@@ -11,6 +11,8 @@ int main()
     {
         vector<int> v(n, 0);
         vector<int> dp(n, 1);
+        vector<int> res(n, 0);
+        int max_pos = 1, max_len = 1;
         for(int i = 0; i < n; i++)
         {
             cin >> v[i];
@@ -22,30 +24,30 @@ int main()
             {
                 if(v[j] > v[i])
                 {
-                    dp[j] = max(dp[j], dp[i] + 1);
+                    if(dp[i] + 1 > dp[j]) // if increment
+                    {
+                        dp[j] = dp[i] + 1;
+                        max_len = dp[j];
+                        max_pos = j;
+                        res[j] = i;
+                    }
                 }
             }
         }
 
-        vector<int> res;
-        res.push_back(0);
-        for(int i = 1; i < n; i++)
+        vector<int> final_res(max_len, 0);
+        int cnt = 0, max_len_bk = max_len;
+        while(max_len_bk--)
         {
-            int next_pos = upper_bound(res.begin() + i, res.end(), v[i]) - res.begin();
-            if(next_pos >= n)
-            {
-                break;
-            }
-            res.push_back(next_pos);
-            i = next_pos - 1;
+            final_res[cnt++] = max_pos;
+            max_pos = res[max_pos];
         }
 
-        // cout << "answer ---------------- \n";
-        int sz = res.size();
-        cout << sz << '\n'; 
-        for(int i = 0; i < sz; i++)
+        cout << max_len << '\n';
+        reverse(final_res.begin(), final_res.end());
+        for(int i = 0; i < max_len; i++)
         {
-            cout << res[i] << ' ';
+            cout << final_res[i] << ' ';
         }
         cout << '\n';
     }
