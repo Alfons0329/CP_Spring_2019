11a596b6a53f5331c4f79caf0c753d5d1b9caba5
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-06-19 15:06:58 +0800

    Solved TLE problem, fix WA

--- Final/kattis/LIS_AC.cpp
+++ Final/kattis/LIS_AC.cpp
@@ -12,9 +12,11 @@ int main()
         vector<int> v(n, 0);
         vector<int> res;
         vector<int> final_res;
+        unordered_map<int, queue<int>> pos;
         for(int i = 0; i < n; i++)
         {
             cin >> v[i];
+            pos[v[i]].push(i);
         }
         
         res.push_back(v[0]);
@@ -27,14 +29,22 @@ int main()
                 max_len++;
                 res.push_back(v[i]);
                 final_res.push_back(i);
+                pos[v[i]].pop();
             }
             else
             {
                 int lb = lower_bound(res.begin(), res.end(), v[i]) - res.begin();
-                res[lb] = v[i];
-                if(lb == max_len - 1)
+                if(lb == max_len - 1) // last one, does not mattr
                 {
+                    res[lb] = v[i];
                     final_res[lb] = i;
+                    pos[v[i]].pop();
+                }
+                else if(lb > pos[res[lb - 1]].front() && lb < pos[res[lb + 1]].front()) // in the right order
+                {
+                    res[lb] = v[i];
+                    final_res[lb] = i;
+                    pos[v[i]].pop();
                 }
             }
         }
@@ -45,12 +55,14 @@ int main()
             cout << final_res[i] << ' ';
         }
         cout << '\n';
-
+        
+        /* 
         for(int i = 0; i < max_len; i++)
         {
             cout << res[i] << ' ';
         }
         cout << '\n';
+        */
     }
     return 0;
 }
